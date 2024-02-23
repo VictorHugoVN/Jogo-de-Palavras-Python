e451ecf5df87d45af9ebf489d0cc5d53f1e227e5
Victor Hugo <victorneivadef8@gmail.com>
2024-02-23 20:39:38 -0300

    Introdução adicionada

---
+++ jogoDePalavras.py
@@ -0,0 +1,14 @@
+
+
+def introducao():
+    print('\n-=-=-= JOGO DE PALAVRAS -=-=-=\n')
+    print('Você deverá adivinhar qual palavra o computador escolheu!\n')
+
+
+
+
+
+
+
+palavras = ('Casa', 'Computador', 'Farmacia', 'Livro', 'Storage')
+introducao()
